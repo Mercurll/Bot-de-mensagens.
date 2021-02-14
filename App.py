f01196cfcc0ef489d7e42faf3c2bc48cdad9feb9
Mercurll <brayancontente@gmail.com>
2021-02-14 20:02:32 -0300

    first commit

---
+++ App.py
@@ -0,0 +1,13 @@
+import keyboard
+import pywhatkit
+import time
+from datetime import datetime
+# 1. Definir para quais contatos enviar.
+contatos = ['+5541984016258']
+# 2. Definir intervalo de envios.
+while len(contatos) >= 1:
+    pywhatkit.sendwhatmsg(contatos[0], 'Hello World.', datetime.now().hour, datetime.now().minute + 1)
+    del contatos[0]
+    time.sleep(60)
+    keyboard.press_and_release('ctrl + w')
+#Testar!
